2233526c9e3219b3c885c15da57f14607825be6c
Gadiomi <112657344+Gadiomi@users.noreply.github.com>
2023-07-26 16:08:55 +0300

    length

--- js/task-01.js
+++ js/task-01.js
@@ -1,11 +1,11 @@
 const categoriesArray = document.querySelectorAll(".item");
-console.log(`Number of categories: ${categoriesArray.lenght} `);
+console.log(`Number of categories: ${categoriesArray.length} `);
 
 categoriesArray.forEach((category) => {
   const titleElem = document.querySelector("li > h2");
   const categoriName = titleElem.textContent;
   console.log(`Category: ${categoriName}`);
 
-  const categorySum = category.querySelectorAll("li").lenght;
+  const categorySum = category.querySelectorAll("li").length;
   console.log(`Elements: ${categorySum}`);
 });
